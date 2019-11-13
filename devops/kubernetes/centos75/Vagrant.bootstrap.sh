89c4603b18c537d1e01f763ea016144414d6f155
Pradeep <pradeep@seleniumframework.com>
2019-11-12 19:22:54 -0500

    few more infra checks added

--- devops/kubernetes/centos75/Vagrant.bootstrap.sh
+++ devops/kubernetes/centos75/Vagrant.bootstrap.sh
@@ -7,6 +7,27 @@
 #       and revert to snapshot if needed
 
 sudo -i
+# Install and actiate iptables and disable firewalld
+yum install iptables-services.x86_64 -y
+systemctl stop firewalld.service
+systemctl disable firewalld.service
+systemctl mask firewalld.service
+systemctl start iptables
+systemctl enable iptables
+systemctl unmask iptables
+iptables -F
+service iptables save
+
+# Install docker from official repo
+yum install -y yum-utils
+yum-config-manager –add-repo \
+https://docs.docker.com/engine/installation/linux/repo_files/centos/docker.repo
+yum makecache fast
+yum install -y docker-engine
+
+swapoff -a
+sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
+
 yum update
 
 sudo bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
@@ -23,6 +44,10 @@ EOF'
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+yum install -y docker kubelet kubeadm kubectl --disableexcludes=kubernetes
 
 systemctl enable --now kubelet
+
+# Ensure it stars upon reboot
+systemctl restart docker && systemctl enable docker
+systemctl restart kubelet && systemctl enable kubelet
