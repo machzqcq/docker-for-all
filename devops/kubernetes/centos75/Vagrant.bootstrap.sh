147808ede2e3b1f996bf053d349d8740b84b8922
Pradeep <pradeep@seleniumframework.com>
2019-11-12 19:28:50 -0500

    enable iptables filtering

--- devops/kubernetes/centos75/Vagrant.bootstrap.sh
+++ devops/kubernetes/centos75/Vagrant.bootstrap.sh
@@ -43,10 +43,15 @@ EOF'
 # Set SELinux in permissive mode (effectively disabling it)
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-
 yum install -y docker kubelet kubeadm kubectl --disableexcludes=kubernetes
 
-systemctl enable --now kubelet
+# enable iptables filtering so proxying works correctly
+sudo bash -c 'cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF'
+ 
+sysctl --system
 
 # Ensure it stars upon reboot
 systemctl restart docker && systemctl enable docker
